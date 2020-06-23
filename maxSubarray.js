3484e7974a73eb3591baa2f4446ce1f9e122c41c
iykekings <iykekings36@gmail.com>
2020-06-23 13:07:10 +0100

    reafctor to use kedane's algorithm

--- maxSubarray.js
+++ maxSubarray.js
@@ -1,41 +1,17 @@
 const assert = require('assert');
 
-function maxSubArraySum(arr) {
-  let store = [];
-  for (let i = 0; i < arr.length; i++) {
-    store[i] = arr.slice(0, i + 1).reduce((a, b) => a + b, 0);
-  }
-  let max = -Infinity;
-  let acc = 0;
-  for (let i = 0; i < store.length; i++) {
-    if (store[i] < 0) {
-      max = Math.max(max, arr[i]);
-      acc = 0;
-    } else {
-      acc = acc + arr[i];
-      max = Math.max(acc, max);
-    }
-  }
-  return max;
-}
-
 function maxSubarray(arr) {
-  return [maxSubArraySum(arr), maxSubsequenceSum(arr)];
-}
-
-function maxSubsequenceSum(arr) {
-  let pos = 0;
-  let neg = -Infinity;
-  let hasPos = false;
-  for (let a of arr) {
-    if (a >= 0) {
-      pos += a;
-      hasPos = true;
-    } else {
-      neg = Math.max(neg, a);
-    }
+  console.time('Time');
+  let maxSubArraySum = -Infinity;
+  let currentSum = 0;
+  let maxSubsequenceSum = -Infinity;
+  for (let x of arr) {
+    maxSubsequenceSum = Math.max(x, maxSubsequenceSum + x, maxSubsequenceSum);
+    currentSum = Math.max(x, currentSum + x);
+    maxSubArraySum = Math.max(maxSubArraySum, currentSum);
   }
-  return hasPos ? pos : neg;
+  console.timeEnd('Time');
+  return [maxSubArraySum, maxSubsequenceSum];
 }
 
 assert.deepStrictEqual(maxSubarray([-1, 2, 3, -4, 5, 10]), [16, 20]);
